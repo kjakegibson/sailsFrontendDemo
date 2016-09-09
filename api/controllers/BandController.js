3e80e237832e287a7a5cb4953474cf7afb00a314
kjakegibson <kjakegibson@hotmail.com>
2016-09-09 16:07:05 -0400

    Final Commit

--- api/controllers/BandController.js
+++ api/controllers/BandController.js
@@ -7,7 +7,7 @@
 
 var Client = require('node-rest-client').Client;
 var client = new Client();
-var endpoint = "http://localhost:1337/employee"
+var endpoint = "http://localhost:1337/band"
 
 module.exports = {
 
@@ -44,9 +44,9 @@ module.exports = {
   read: function (req, res) {
 
     client.get(endpoint, function (data, response) {
-        return res.view('read', {employees: data});
+        return res.view('read', {bands: data});
     }).on('error', function (err) {
-        return res.view('read', {error: { message: "There was an error getting the employees"}});
+        return res.view('read', {error: { message: "There was an error getting the bands"}});
     });
 
   },
@@ -56,9 +56,22 @@ module.exports = {
    * `EmployeeController.update()`
    */
   update: function (req, res) {
-    return res.json({
-      todo: 'update() is not implemented yet!'
-    });
+       if(req.method != "POST"){
+      return res.view('update');
+    }
+    var args = {
+        data: req.body,
+        headers: { "Content-Type": "application/json" }
+    };
+    var bandid = req.allParams();
+    var endpointUP = endpoint + "/" + bandid.id;
+    client.put(endpointUP, args, function (data, response) {
+        if(response.statusCode != "200"){
+            return res.view('update', {error:{message: response.statusMessage + ": " + data.reason}});
+        }
+      
+        return res.view('update', {success:{message: "Record updated successfully"}});
+    })
   },
 
 
@@ -66,9 +79,22 @@ module.exports = {
    * `EmployeeController.delete()`
    */
   delete: function (req, res) {
-    return res.json({
-      todo: 'delete() is not implemented yet!'
-    });
+    if(req.method != "POST"){
+      return res.view('delete');
+    }
+    var args = {
+        data: req.body,
+        headers: { "Content-Type": "application/json" }
+    };
+    var bandid = req.allParams();
+    var endpointUP = endpoint + "/" + bandid.id;
+    client.delete(endpointUP, args, function (data, response) {
+        if(response.statusCode != "200"){
+            return res.view('delete', {error:{message: response.statusMessage + ": " + data.reason}});
+        }
+      
+        return res.view('delete', {success:{message: "Record deleted successfully"}});
+    })
   }
 };
 
--- config/routes.js
+++ config/routes.js
@@ -32,10 +32,10 @@ module.exports.routes = {
   *                                                                          *
   ***************************************************************************/
 
-  '/create':'EmployeeController.create',
-  '/'      :'EmployeeController.read',
-  '/update':'EmployeeController.update',
-  '/delete':'EmployeeController.delete'
+  '/create':'BandController.create',
+  '/'      :'BandController.read',
+  '/update':'BandController.update',
+  '/delete':'BandController.delete'
 
   /***************************************************************************
   *                                                                          *
